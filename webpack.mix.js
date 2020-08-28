aa993b115b033df8c332f41ecc71b523bfb04ce9
Miller Kisley <milkisly@gmail.com>
2020-08-28 19:56:33 -0300

    alterações do fronte do site e login

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copyDirectory('node_modules/startbootstrap-agency/dist/assets/img', 'public/assets/img')
+    .styles('node_modules/startbootstrap-agency/dist/css/styles.css', 'public/css/styles.css')
+    .styles('resources/css/login.css', 'public/css/login.css')
+    .styles('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css')
+    .scripts('node_modules/jquery/dist/jquery.min.js', 'public/scripts/jquery.min.js')
+    .scripts('node_modules/jquery/dist/jquery.slim.min.js', 'public/scripts/jquery.slim.min.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/scripts/bootstrap.bundle.min.js')
+    .scripts('node_modules/@fortawesome/fontawesome-free/js/all.min.js', 'public/scripts/all.min.js')
+    .scripts('node_modules/startbootstrap-agency/dist/assets/mail/jqBootstrapValidation.js', 'public/scripts/jqBootstrapValidation.js')
+    .scripts('node_modules/startbootstrap-agency/dist/assets/mail/contact_me.js', 'public/scripts/contact_me.js')
+    .scripts('node_modules/startbootstrap-agency/dist/js/scripts.js', 'public/scripts/scripts.js');
