2247a08857a0c5c6cb319b1ca390ab3f386d5cd1
Miller Kisley <milkisly@gmail.com>
2020-09-03 11:05:50 -0300

    criação do templete dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,139 +1,67 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="{{ asset('dash/assets/img/favicon.ico') }}">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+@extends('dashboard.layouts.master')
 
-	<title>Light Bootstrap Dashboard by Creative Tim</title>
+@section('content')
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+<div class="col-md-8">
+    <div class="card">
+        <div class="header">
+            <h4 class="title">Editar Perfil</h4>
+        </div>
+        <div class="content">
+            <form>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" placeholder="Email" disabled>
+                        </div>
+                    </div>
+                </div>
 
-    <!-- Bootstrap core CSS     -->
-    <link href="{{ asset('dash/css/bootstrap.min.css') }}" rel="stylesheet" />
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" class="form-control" placeholder="Nome">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Senha</label>
+                            <input type="text" class="form-control" placeholder="Senha">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Repetir Senha</label>
+                            <input type="text" class="form-control" placeholder="Repetir Senha">
+                        </div>
+                    </div>
+                </div>
 
-    <!-- Animation library for notifications   -->
-    <link href="{{ asset('dash/css/animate.min.css') }}" rel="stylesheet"/>
+                <button type="submit" class="btn btn-info btn-fill pull-right">Atualizar Perfil</button>
+                <div class="clearfix"></div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="col-md-4">
+    <div class="card card-user">
+        <div class="image">
+            <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&amp;fm=jpg&amp;h=300&amp;q=75&amp;w=400" alt="...">
+        </div>
+        <div class="content">
+            <div class="author">
+                    <a href="#">
+                <img class="avatar border-gray" src="{{ asset('dash/assets/img/faces/face-6.jpg') }}" alt="...">
 
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="{{ asset('dash/css/light-bootstrap-dashboard.css') }}" rel="stylesheet"/>
-
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="{{ asset('dash/css/pe-icon-7-stroke.css') }}" rel="stylesheet" />
-</head>
-<body>
-
-<div class="wrapper">
-    <div class="sidebar" data-color="purple" data-image="{{ asset('dash/assets/img/sidebar-5.jpg') }}">
-
-    <!--
-
-        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
-        Tip 2: you can also add an image using data-image tag
-
-    -->
-
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="http://www.creative-tim.com" class="simple-text">
-                    Creative Tim
+                    <h4 class="title">Mike Andrew<br>
+                        <small>michael24@mail.com</small>
+                    </h4>
                 </a>
             </div>
-
-            <ul class="nav">
-                <li class="active">
-                    <a href="dashboard.html">
-                        <i class="pe-7s-graph"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-
-            </ul>
-    	</div>
-    </div>
-
-    <div class="main-panel">
-        <nav class="navbar navbar-default navbar-fixed">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Dashboard</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-dashboard"></i>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                           <a href="">
-                               Account
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-
-                </div>
-            </div>
         </div>
-
-
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
-                </p>
-            </div>
-        </footer>
-
     </div>
 </div>
 
-
-</body>
-    <!--   Core JS Files   -->
-    <script src="{{ asset('dash/scripts/jquery.3.2.1.min.js') }}"></script>
-
-	<script src="{{ asset('dash/scripts/bootstrap.min.js') }}" type="text/javascript"></script>
-
-	<!--  Charts Plugin -->
-	<script src="{{ asset('dash/scripts/chartist.min.js') }}"></script>
-
-    <!--  Notifications Plugin    -->
-    <script src="{{ asset('dash/scripts/bootstrap-notify.js') }}"></script>
-
-    <!--  Google Maps Plugin    -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-
-    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-	<script src="{{ asset('dash/scripts/light-bootstrap-dashboard.js') }}"></script>
-</html>
+@endsection
